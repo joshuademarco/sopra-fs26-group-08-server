1ea67175e33e9855c753ce7bd3175a82b557f8dd
Alessandro <alessandromichele.caputo@uzh.ch>
2026-04-24 12:54:41 +0200

    leaderboard tests

--- src/test/java/ch/uzh/ifi/hase/soprafs26/service/UserServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/service/UserServiceTest.java
@@ -3,6 +3,10 @@ package ch.uzh.ifi.hase.soprafs26.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.any;
@@ -19,6 +23,7 @@ import ch.uzh.ifi.hase.soprafs26.entity.Character;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.repository.CharacterRepository;
 import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.LeaderboardEntryDTO;
 
 public class UserServiceTest {
 
@@ -98,4 +103,35 @@ public class UserServiceTest {
 		assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser));
 	}
 
+    @Test
+    public void getLeaderboard_returnsSortedAndFilteredLeaderboard() {
+        User userLow = new User();
+        userLow.setUsername("lowUser");
+        Character lowCharacter = new Character();
+        lowCharacter.setExperience(10);
+        lowCharacter.setLevel(1);
+        userLow.setCharacter(lowCharacter);
+
+        User userHigh = new User();
+        userHigh.setUsername("highUser");
+        Character highCharacter = new Character();
+        highCharacter.setExperience(250);
+        highCharacter.setLevel(5);
+        userHigh.setCharacter(highCharacter);
+
+        User noCharacterUser = new User();
+        noCharacterUser.setUsername("noCharacterUser");
+
+        Mockito.when(userRepository.findAll()).thenReturn(Arrays.asList(userLow, noCharacterUser, userHigh));
+
+        List<LeaderboardEntryDTO> leaderboard = userService.getLeaderboard();
+
+        assertEquals(2, leaderboard.size());
+        assertEquals("highUser", leaderboard.get(0).getUsername());
+        assertEquals(250, leaderboard.get(0).getExperience());
+        assertEquals(5, leaderboard.get(0).getLevel());
+        assertEquals("lowUser", leaderboard.get(1).getUsername());
+        assertEquals(10, leaderboard.get(1).getExperience());
+        assertEquals(1, leaderboard.get(1).getLevel());
+    }
 }
