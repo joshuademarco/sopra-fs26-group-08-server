e66b4837387c7b500ebbced313410e520c6846f8
Léonard Ménard <leonard.menard5@gmail.com>
2026-05-01 18:44:10 +0200

    feat: implement achievement system with streak tracking and habit reset scheduler

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/constant/AchievementKey.java
@@ -0,0 +1,11 @@
+package ch.uzh.ifi.hase.soprafs26.constant;
+
+public enum AchievementKey {
+    FIRST_HABIT,
+    STREAK_3,
+    STREAK_7,
+    //FIRST_BOSS, once boss raid complete
+    STRENGTH_25,
+    INTELLIGENCE_25,
+    RESILIENCE_25
+}
