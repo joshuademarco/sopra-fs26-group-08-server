90c977ca148deff18822e803d7bd3924ba29019e
Joshua De Marco <joshua.dm@hispeed.ch>
2026-05-04 11:49:04 +0200

    added manual cors filter in hope for ti to solve the cors issues and double tokens

--- src/main/java/ch/uzh/ifi/hase/soprafs26/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/Application.java
@@ -7,6 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @RestController
 @SpringBootApplication
@@ -36,5 +41,25 @@ public class Application {
 						.exposedHeaders("Set-Cookie");
 			}
 		};
+	}
+
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOriginPatterns(Arrays.asList("https://bettertogeter.ch", "https://www.bettertogeter.ch", "http://localhost:3000"));
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("PATCH");
+		config.addAllowedHeader("*");
+		config.setExposedHeaders(Arrays.asList("Set-Cookie"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
